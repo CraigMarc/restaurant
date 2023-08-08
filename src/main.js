ec3f8c1e2d88da0d2b68880d655f51e74c6f3063
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-08 16:17:30 -0400

    added pics main.js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function mainPage() {
+
+const main = document.querySelector('#content');
+
+const heading = document.createElement('div');
+heading.classList.add('heading');
+heading.textContent = "Heading";
+
+container.appendChild(heading);
+
+
+
+
+}
