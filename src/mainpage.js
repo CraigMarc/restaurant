1a9fea2bce4d866e6719c1304a80b7dc427ce1ac
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-08 22:37:33 -0400

    stuff

--- src/mainpage.js
+++ src/mainpage.js
@@ -3,17 +3,22 @@ import Icon from './images/pizza.jpeg';
 function component() {
     const element = document.createElement('div');
     
-  
-    // Lodash, now imported by this script
     element.textContent = 'Hello Module';
+
+       
+
+    const element2 = document.createElement('div');
+    
 
     const myIcon = new Image();
     myIcon.src = Icon;
   
-    element.appendChild(myIcon);
-   
+    element2.appendChild(myIcon);
+    myIcon.classList.add('pic1');
+   document.body.appendChild(element2);
    
     return document.body.appendChild(element);
+
   }
 
   export default component;
