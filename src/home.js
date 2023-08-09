56f5ee7be9f1df56b42126edf1c6f69960ba8c61
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-09 17:24:24 -0400

    added main page

---
+++ src/home.js
@@ -0,0 +1,59 @@
+import Icon from './images/pizza.jpeg'
+
+function home() {
+    const div1 = document.createElement('div');
+    div1.classList.add('main');
+
+    const div2 = document.createElement('div');
+    const p1 = document.createElement('div');
+    p1.textContent = '"The best pizza I have ever eaten" Pisquales Customer';
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    const div3 = document.createElement('div')
+    div3.classList.add('hours');
+
+    const h3 = document.createElement('h3')
+    h3.textContent = "Hours"
+
+    const hours1 = document.createElement('h4')
+    hours1.textContent = "Sunday: 11am - 8pm"
+
+    const hours2 = document.createElement('h4')
+    hours2.textContent = "Monday: 11am - 8pm"
+
+    const hours3 = document.createElement('h4')
+    hours3.textContent = "Tuesday: Closed"
+
+    const hours4 = document.createElement('h4')
+    hours4.textContent = "Wednesday: 11am - 8pm"
+
+    const hours5 = document.createElement('h4')
+    hours5.textContent = "Thursday: 11am - 8pm"
+
+    const hours6 = document.createElement('h4')
+    hours6.textContent = "Friday: 1pm - 10pm"
+
+    const hours7 = document.createElement('h4')
+    hours7.textContent = "Saturday: 1pm - 10pm"
+
+
+    div1.appendChild(div2);
+    div1.appendChild(div3);
+    div2.appendChild(myIcon)
+    div2.appendChild(p1)
+    div3.appendChild(hours1)
+    div3.appendChild(hours2)
+    div3.appendChild(hours3)
+    div3.appendChild(hours4)
+    div3.appendChild(hours5)
+    div3.appendChild(hours6)
+    div3.appendChild(hours7)
+
+
+
+    return document.body.appendChild(div1);
+
+}
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './styles.css';
 
 import header from './header';
 
- 
+import home from './home';
 
    header()
+   home()
