27bcb0f9f52cebb0252dbafa5d63b2d5eb186821
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-10 12:06:14 -0400

    added buttton line

--- src/index.js
+++ src/index.js
@@ -24,20 +24,32 @@ select.forEach((button) => {
 
 function update (e) {
 
+const homeClick = document.getElementById("home")
+const menuClick = document.getElementById("menu")
+const locationClick = document.getElementById("location")
 
 const remove = document.getElementById("remove");
 
 if (e.target.id == 'home') {
+   homeClick.style.borderBottom = 'solid'; 
+   menuClick.style.borderBottom = 'none'; 
+   locationClick.style.borderBottom = 'none'; 
    remove.remove()
    home()
 }
 
 if (e.target.id == 'menu') {
+   homeClick.style.borderBottom = 'none'; 
+   menuClick.style.borderBottom = 'solid'; 
+   locationClick.style.borderBottom = 'none'; 
    remove.remove()
    menu()
 }
 
 if (e.target.id == 'location') {
+   homeClick.style.borderBottom = 'none'; 
+   menuClick.style.borderBottom = 'none'; 
+   locationClick.style.borderBottom = 'solid'; 
    remove.remove()
    location()
 }
